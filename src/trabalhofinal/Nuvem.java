924ed16ef3e67780e990b67231abcb5a26e0689f
jodaltro <kinst2009@hotmail.com>
2015-11-10 02:31:43 -0200

    Fault Recovery added when a one User is gonna be registered.

--- src/trabalhofinal/Nuvem.java
+++ src/trabalhofinal/Nuvem.java
@@ -18,175 +18,164 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 
-
 /**
+ * @author Jodaltro
  *
- * @author kinst
  */
 public class Nuvem {
 
-    public static ArrayList<No> nos;
-    private static int port = 8080;
-    private static URL bootstrapURL = null;
-    public static No nodoaux = null;
-    public static ChordImpl chord = null; 
-    private static int nNos = 0;
+	public static ArrayList<No> nos;
+	public static int port = 8080;
+	private static URL bootstrapURL = null;
+	public static No nodoaux = null;
+	public static ChordImpl chord = null;
+	private static int nNos = 0;
 
-   
-    
-    public static void newUser(String _user, String _password)
-    {
-       /* File arquivo = null;
-        String dir = null;
-        StringKey myKEY = null;*/
-        User user = null;
-        user = new User();
-            user.setName(_user);
-            user.setPassword(_password);
-            Nuvem.addPassword(user, nodoaux);
-    }
-    
-    public static boolean createNetwork()
-    {
-        de.uniba.wiai.lspi.chord.service.PropertiesLoader.loadPropertyFile();
-        Nuvem.chord = new de.uniba.wiai.lspi.chord.service.impl.ChordImpl();
-       nos = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            nos.add(new No(port, nNos));
-            if (port == 8080) {
-                bootstrapURL = nos.get(0).getAdress();
-                setNetwork(nos.get(0).getNo(), nos.get(0).getAdress());
-            } else {
-                setNodo(nos.get(i).getNo(), nos.get(i).getAdress(), bootstrapURL);
-            }
-            System.out.println("Criado No:" + (i) + " Porta:" + nos.get(i).getPorta() + " Id: " + nos.get(i).getID());
-            Nuvem.port++;
-            Nuvem.nNos++;
-        }
-        return true;
-    }
+	public static void newUser(String _user, String _password) {
+		User user = null;
+		user = new User();
+		user.setName(_user.trim()); // trim to remove spaces
+		user.setPassword(_password);
+		Nuvem.addPassword(user, nodoaux);
+	}
 
-    public static void setNetwork(ChordImpl begin, URL url) {
-        try {
-            begin.create(url);
-        } catch (ServiceException e) {
-            throw new RuntimeException("Could not create DHT !", e);
-        }
-    }
+	public static boolean createNetwork() {
+		nos = new ArrayList<>();
+		try {
+			for (int i = 0; i < 5; i++) {
+				nos.add(new No(port, nNos));
+				if (port == 8080) {
+					bootstrapURL = nos.get(0).getAdress();
+					setNetwork(nos.get(0).getNo(), nos.get(0).getAdress());
+				} else {
+					setNodo(nos.get(i).getNo(), nos.get(i).getAdress(), bootstrapURL);
+				}
+				System.out
+						.println("Criado No:" + (i) + " Porta:" + nos.get(i).getPorta() + " Id: " + nos.get(i).getID());
+				Nuvem.port++;
+				Nuvem.nNos++;
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
-    public static void setNodo(ChordImpl begin, URL url, URL boot) {
-        try {
-            begin.join(url, boot);
-        } catch (ServiceException e) {
-            throw new RuntimeException(" Could not join DHT !", e);
-        }
-    }
-    
-    public static void addPassword(User user, No no)
-    {
-        user.breakPassword(user.encryptPassword(user.getPassword()));
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt1());
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt2());
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt3());
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt4());
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt5());
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt6());
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt7());
-        no.getNo().insert(new StringKey(user.getName()), user.getPassPt8());
-        no.getNo().insert(new StringKey(user.getPassword()), user.getPassPt8());
-    }
-    
-    @SuppressWarnings("resource")
+	public static void setNetwork(ChordImpl begin, URL url) {
+		try {
+			begin.create(url);
+		} catch (ServiceException e) {
+			throw new RuntimeException("Could not create DHT !", e);
+		}
+	}
+
+	public static void setNodo(ChordImpl begin, URL url, URL boot) {
+		try {
+			begin.join(url, boot);
+		} catch (ServiceException e) {
+			throw new RuntimeException(" Could not join DHT !", e);
+		}
+	}
+
+	public static void addPassword(User user, No no) {
+		user.breakPassword(user.encryptPassword(user.getPassword()));
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt1());
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt2());
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt3());
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt4());
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt5());
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt6());
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt7());
+		no.getNo().insert(new StringKey(user.getName()), user.getPassPt8());
+		no.getNo().insert(new StringKey(user.getPassword()), user.getPassPt8());
+	}
+
+	@SuppressWarnings("resource")
 	public static void createArchive(File source, File destination) throws IOException {
-        if (destination.exists()) {
-            destination.delete();
-        }
+		if (destination.exists()) {
+			destination.delete();
+		}
 
-        FileChannel sourceChannel = null;
-        FileChannel destinationChannel = null;
+		FileChannel sourceChannel = null;
+		FileChannel destinationChannel = null;
 
-        try {
-            sourceChannel = new FileInputStream(source).getChannel();
-            destinationChannel = new FileOutputStream(destination).getChannel();
-            sourceChannel.transferTo(0, sourceChannel.size(),
-                    destinationChannel);
-        } finally {
-            if (sourceChannel != null && sourceChannel.isOpen()) {
-                sourceChannel.close();
-            }
-            if (destinationChannel != null && destinationChannel.isOpen()) {
-                destinationChannel.close();
-            }
-        }
-    }
+		try {
+			sourceChannel = new FileInputStream(source).getChannel();
+			destinationChannel = new FileOutputStream(destination).getChannel();
+			sourceChannel.transferTo(0, sourceChannel.size(), destinationChannel);
+		} finally {
+			if (sourceChannel != null && sourceChannel.isOpen()) {
+				sourceChannel.close();
+			}
+			if (destinationChannel != null && destinationChannel.isOpen()) {
+				destinationChannel.close();
+			}
+		}
+	}
 
-    //
-    /**
-     * @param _name
-     * @return
-     * Create a hash key from a name of user
-     */
-    public StringKey getKey(String _name) {
-        return new StringKey(_name);
-    }
-    
-    
-    /**
-     * @param _user
-     * @param _password
-     * @param no
-     * @return
-     * Method called by company that need validate a user in your system
-     */
-    public static String validateUser(String _user, String _password ,Nodo no)
-    {
-        String codePass = null;
-        String aux = null;
-        String aux2 = null;
-        User user = new User();
-        StringKey myKey = new StringKey(_user);
-        Set<Serializable> vals = null;
-        vals = no.getChord().retrieve(myKey);
-        Iterator<Serializable> it = vals.iterator();
-        while (it.hasNext()) {
-            aux = (String) it.next();
-            aux2 = aux.substring(aux.length() - 1, aux.length());
-            switch (aux2) {
-                case "1":
-                    user.setPassPt1(aux.substring(0, aux.length() - 1));
-                    continue;
-                case "2":
-                    user.setPassPt2(aux.substring(0, aux.length() - 1));
-                    continue;
-                case "3":
-                    user.setPassPt3(aux.substring(0, aux.length() - 1));
-                    continue;
-                case "4":
-                    user.setPassPt4(aux.substring(0, aux.length() - 1));
-                    continue;
-                case "5":
-                    user.setPassPt5(aux.substring(0, aux.length() - 1));
-                    continue;
-                case "6":
-                    user.setPassPt6(aux.substring(0, aux.length() - 1));
-                    continue;
-                case "7":
-                    user.setPassPt7(aux.substring(0, aux.length() - 1));
-                    continue;
-                case "8":
-                    user.setPassPt8(aux.substring(0, aux.length() - 1));
-                    continue;
-            }
-        }
-        if (user.unitePassword().equals(user.encryptPassword(_password))) {
-            myKey = new StringKey(_password);
-            vals = no.getChord().retrieve(myKey);
-            it = vals.iterator();
-            while (it.hasNext()) {
-                codePass = (String) it.next();
-                break;
-            }
-        }
-        return codePass;
-    }
+	//
+	/**
+	 * @param _name
+	 * @return Create a hash key from a name of user
+	 */
+	public StringKey getKey(String _name) {
+		return new StringKey(_name);
+	}
+
+	/**
+	 * @param _user
+	 * @param _password
+	 * @param no
+	 * @return Method called by company that need validate a user in your system
+	 */
+	public static String validateUser(String _user, String _password) {
+		String codePass = null;
+		String aux = null;
+		String aux2 = null;
+		User user = new User();
+		StringKey myKey = new StringKey(_user.trim());
+		Set<Serializable> vals = null;
+		vals = nodoaux.getNo().retrieve(myKey);
+		Iterator<Serializable> it = vals.iterator();
+		while (it.hasNext()) {
+			aux = (String) it.next();
+			aux2 = aux.substring(aux.length() - 1, aux.length());
+			switch (aux2) {
+			case "1":
+				user.setPassPt1(aux.substring(0, aux.length() - 1));
+				continue;
+			case "2":
+				user.setPassPt2(aux.substring(0, aux.length() - 1));
+				continue;
+			case "3":
+				user.setPassPt3(aux.substring(0, aux.length() - 1));
+				continue;
+			case "4":
+				user.setPassPt4(aux.substring(0, aux.length() - 1));
+				continue;
+			case "5":
+				user.setPassPt5(aux.substring(0, aux.length() - 1));
+				continue;
+			case "6":
+				user.setPassPt6(aux.substring(0, aux.length() - 1));
+				continue;
+			case "7":
+				user.setPassPt7(aux.substring(0, aux.length() - 1));
+				continue;
+			case "8":
+				user.setPassPt8(aux.substring(0, aux.length() - 1));
+				continue;
+			}
+		}
+		if (user.unitePassword().equals(user.encryptPassword(_password))) {
+			myKey = new StringKey(_password);
+			vals = nodoaux.getNo().retrieve(myKey);
+			it = vals.iterator();
+			while (it.hasNext()) {
+				codePass = (String) it.next();
+				break;
+			}
+		}
+		return codePass;
+	}
 }
--- src/trabalhofinal/jgroups.java
+++ src/trabalhofinal/jgroups.java
@@ -11,103 +11,98 @@ import java.util.Scanner;
 import java.util.LinkedList;
 
 public class jgroups extends ReceiverAdapter {
-    JChannel channel;
-    String user_name=System.getProperty("user.name", "n/a");
-    final List<String> state=new LinkedList<String>();
-    public void viewAccepted(View new_view) {
-        System.out.println("** view: " + new_view);
-    }
+	JChannel channel;
+	String user_name = System.getProperty("user.name", "n/a");
+	final List<String> state = new LinkedList<String>();
 
-    public void receive(Message msg) {
-        String line=msg.getSrc() + ": " + msg.getObject();
-       // System.out.println(line);
-       
-        synchronized(state) {
-            state.add(line);
-        }
-    }
+	public void viewAccepted(View new_view) {
+		System.out.println("** view: " + new_view);
+	}
 
-    public byte[] getState() {
-        synchronized(state) {
-            try {
-                return Util.objectToByteBuffer(state);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-    }
+	public void receive(Message msg) {
+		String line = msg.getSrc() + ": " + msg.getObject();
+		// System.out.println(line);
 
-    @SuppressWarnings("unchecked")
+		synchronized (state) {
+			state.add(line);
+		}
+	}
+
+	public byte[] getState() {
+		synchronized (state) {
+			try {
+				return Util.objectToByteBuffer(state);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
 	public void setState(byte[] new_state) {
-        try {
-            List<String> list=(List<String>)Util.objectFromByteBuffer(new_state);
-            synchronized(state) {
-                state.clear();
-                state.addAll(list);
-            }
-            System.out.println("received state (" + list.size() + " messages in chat history):");
-            for(String str: list) {
-                System.out.println(str);
-            }
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			List<String> list = (List<String>) Util.objectFromByteBuffer(new_state);
+			synchronized (state) {
+				state.clear();
+				state.addAll(list);
+			}
+			System.out.println("received state (" + list.size() + " messages in chat history):");
+			for (String str : list) {
+				System.out.println(str);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void start() throws Exception {
-        channel=new JChannel();
-        channel.setReceiver(this);
-        channel.connect("nuvem");
-        channel.getState(null, 10000);
-        eventLoop();
-        channel.close();
-    }
+	public void start() throws Exception {
+		channel = new JChannel();
+		channel.setReceiver(this);
+		channel.connect("nuvem");
+		channel.getState(null, 10000);
+		eventLoop();
+		channel.close();
+	}
 
-    @SuppressWarnings("resource")
-	private void eventLoop() {
-       // BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-    	int cont = 0;
-        while(true) {
-            try {
-            	System.out.println("Cadastrar novo Usuario!");
-                System.out.println("Login:");
-                String usuario = new Scanner(System.in).nextLine();
-                System.out.println("Senha:");
-                String senha = new Scanner(System.in).nextLine();
-                Nuvem.newUser(usuario,senha);
-                for (int x = 0; x < Nuvem.nos.size(); x++) {
-                            System.out.println("***No; " + Nuvem.nos.get(x).getIdAux());
-                            System.out.println("-- " + Nuvem.nos.get(x).getID() + " " + Nuvem.nos.get(x).getAdress());
-                            System.out.println("-->" + Nuvem.nos.get(x).getNo().printFingerTable());
-                            System.out.println("->" + Nuvem.nos.get(x).getNo().printEntries());
-                            System.out.println("\n");
-                }
-                //System.out.println(mainDHT.validaAqui(usuario, senha));
-       
-               // System.out.print("> "); 
-                //System.out.flush();
-                //String line=in.readLine().toLowerCase();
-                /*if(line.startsWith("quit") || line.startsWith("exit")) {
-                    break;
-                }*/
-                usuario=usuario +":"+ senha +":"+cont;
-                cont++;
-                Message msg=new Message(null, null, usuario);
-             
-               // String duvido = 
-               // System.out.println();
-                channel.send(msg);
-               // System.out.println("aqui foi"+msg.toString());
-                
-               
-                
-            }
-            catch(Exception e) {
-            }
-        }
-    }
+	@SuppressWarnings("resource")
+	private void eventLoop() throws Exception {
+		// BufferedReader in=new BufferedReader(new
+		// InputStreamReader(System.in));
+		int cont = 0;
+		boolean flag = true; // is false, access the reserve host
+		String user_Pass = null;
+		while (true) {
+			try {
+				System.out.println("Sign up new user!");
+				System.out.println("Login:");
+				String user = new Scanner(System.in).nextLine();
+				System.out.println("Password:");
+				String password = new Scanner(System.in).nextLine();
+				user_Pass = user + ":" + password + ":" + cont;
+				cont++;
+				Nuvem.newUser(user, password);
+				for (int x = 0; x < Nuvem.nos.size(); x++) {
+					System.out.println("***No; " + Nuvem.nos.get(x).getIdAux());
+					System.out.println("-- " + Nuvem.nos.get(x).getID() + " " + Nuvem.nos.get(x).getAdress());
+					System.out.println("-->" + Nuvem.nos.get(x).getNo().printFingerTable());
+					System.out.println("->" + Nuvem.nos.get(x).getNo().printEntries());
+					System.out.println("\n");
+				}
+				Message msg = new Message(null, null, user_Pass);
+				channel.send(msg);
+
+			} catch (Exception e) {
+				System.out.println("Problem to conect to network\n");
+				flag = false;
+			}
+			if (!flag) {
+				flag = true;
+				System.out.println("Trying to connect the auxiliary network...");
+				Message msg = new Message(null, null, user_Pass);
+				channel.send(msg);
+				System.out.println("User added only in auxiliary network!");
+			}
+		}
+	}
 }
-
